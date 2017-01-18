5116a4d2bd19e41f9771ac01a2f4f05df8a7a72a
Fernando Küpper <fernando@kupper.com.br>
2017-01-18 13:18:46 +0000

    Passed pair wise product

--- src/algorithmic_toolbox/max_pairwise_product.js
+++ src/algorithmic_toolbox/max_pairwise_product.js
@@ -27,7 +27,7 @@ function readLine(line) {
     let sequence = input[1].toString().split(' ').map(number => parseInt(number, 10));
 
     if (sequence.length === length) {
-      console.log(maxPairWiseProduct(sequence));
+      console.log(maxPairWiseProductFastWhile(sequence));
     }
 
     process.exit();
@@ -36,8 +36,9 @@ function readLine(line) {
 
 }
 
-
-const maxPairWiseProduct = (sequence) => {
+//The first implementation, intentionally made slow
+//Failed case #4/17: time limit exceeded (Time used: 10.07/5.00, memory used: 1303912448/536870912.)
+const maxPairWiseProductSlow = (sequence) => {
   
   let result = 0;
 
@@ -58,4 +59,96 @@ const maxPairWiseProduct = (sequence) => {
 
 };
 
-module.exports = maxPairWiseProduct;
+//The second implementation, a little faster using foreach
+//(Max time used: 0.33/5.00, max memory used: 52273152/536870912.)
+const maxPairWiseProductFastForEach = (sequence) => {
+  
+  let max_i = -1;
+  let max_j = -1;
+
+  sequence.forEach(
+    function(item, index) {
+      if( max_i < 0 || item > sequence[max_i]  )
+      {
+        max_i = index;
+      }
+    }
+  );
+  
+  sequence.forEach(
+    function(item, index) {
+      if( (index != max_i) && ( max_j < 0 || item > sequence[max_j]  ) )
+      {
+        max_j = index;
+      }
+    }
+  );
+  return sequence[max_i] * sequence[max_j];
+
+};
+
+//The second implementation, a little faster using for
+//(Max time used: 0.23/5.00, max memory used: 39251968/536870912.)
+const maxPairWiseProductFastFor = (sequence) => {
+  
+  let max_i = -1;
+  let max_j = -1;
+
+
+  for (var i = 0; i < sequence.length; i++) 
+  {
+    if( max_i < 0 || sequence[i] > sequence[max_i]  )
+    {
+      max_i = i;
+    }
+  }
+  
+  for (var j = 0; j < sequence.length; j++) 
+  {
+    if( (j !== max_i) && (max_j < 0 || sequence[j] > sequence[max_j])  )
+    {
+      max_j = j;
+    }
+  }
+  
+  return sequence[max_i] * sequence[max_j];
+};
+
+
+//The second implementation, a little faster using while
+//(Max time used: 0.23/5.00, max memory used: 39227392/536870912.)
+const maxPairWiseProductFastWhile = (sequence) => {
+  
+  let max_i = -1;
+  let max_j = -1;
+
+  var i = 0;
+  while (  i < sequence.length  )
+  {
+    if( max_i < 0 || sequence[i] > sequence[max_i]  )
+    {
+      max_i = i;
+    }
+    i++;
+  }
+  
+  var j = 0;
+  while (  j < sequence.length  )
+  {
+    if( (j !== max_i) && (max_j < 0 || sequence[j] > sequence[max_j])  )
+    {
+      max_j = j;
+    } 
+    j++;
+  }
+
+  return sequence[max_i] * sequence[max_j];
+};
+
+
+module.exports = {
+    slow: maxPairWiseProductSlow,
+    fastForEach: maxPairWiseProductFastForEach,
+    fastFor: maxPairWiseProductFastFor,
+    fastWhile: maxPairWiseProductFastWhile
+}
--- test/max_pairwise_product_test.js
+++ test/max_pairwise_product_test.js
@@ -1,20 +1,106 @@
 var maxPairWiseProduct = require('../src/algorithmic_toolbox/max_pairwise_product');
 var expect = require('chai').expect;
 
-describe('maxPairWiseProduct', () => {
+var simplestCase = [[1,2,3,4], 12];
+var firstGreater = [[4,3,1,2], 12];
+var duplicate = [[1,3,3], 9];
+var bigNumbers = [[100000, 90000],9000000000 ];
+var hugeDataset = [new Array(10000).join('0').split('').map(parseFloat), 0];
+
+describe('maxPairWiseProductSlow', () => {
 
     it('should work on simplest case', () => {
-        expect(maxPairWiseProduct([1, 2, 3, 4])).to.equal(12);
+        expect(maxPairWiseProduct.slow(simplestCase[0])).to.equal(simplestCase[1]);
     });
 
     it('should work on duplicate inputs', () => {
-        expect(maxPairWiseProduct([1, 3, 3])).to.equal(9);
+        expect(maxPairWiseProduct.slow(duplicate[0])).to.equal(duplicate[1]);
     });
 
     it('should work with big numbers', () => {
-        expect(maxPairWiseProduct([100000, 90000])).to.equal(9000000000);
+        expect(maxPairWiseProduct.slow(bigNumbers[0])).to.equal(bigNumbers[1]);
     });
+    
+    it('should work with the first greater numbers', () => {
+        expect(maxPairWiseProduct.slow(firstGreater[0])).to.equal(firstGreater[1]);
+    });
+
+    it('should work with huge dataset', () => {
+        expect(maxPairWiseProduct.slow(hugeDataset[0])).to.equal(hugeDataset[1]);
+    }).timeout(10000);
+    
+});
+
+describe('maxPairWiseProductFastForEach', () => {
+
+    it('should work on simplest case', () => {
+        expect(maxPairWiseProduct.fastForEach(simplestCase[0])).to.equal(simplestCase[1]);
+    });
+
+    it('should work on duplicate inputs', () => {
+        expect(maxPairWiseProduct.fastForEach(duplicate[0])).to.equal(duplicate[1]);
+    });
+
+    it('should work with big numbers', () => {
+        expect(maxPairWiseProduct.fastForEach(bigNumbers[0])).to.equal(bigNumbers[1]);
+    });
+    
+    it('should work with the first greater numbers', () => {
+        expect(maxPairWiseProduct.fastForEach(firstGreater[0])).to.equal(firstGreater[1]);
+    });
+    
+    it('should work with huge dataset', () => {
+        expect(maxPairWiseProduct.fastForEach(hugeDataset[0])).to.equal(hugeDataset[1]);
+    }).timeout(10000);
+
+});
+
+describe('maxPairWiseProductFastFor', () => {
+
+    it('should work on simplest case', () => {
+        expect(maxPairWiseProduct.fastFor(simplestCase[0])).to.equal(simplestCase[1]);
+    });
+
+    it('should work on duplicate inputs', () => {
+        expect(maxPairWiseProduct.fastFor(duplicate[0])).to.equal(duplicate[1]);
+    });
+
+    it('should work with big numbers', () => {
+        expect(maxPairWiseProduct.fastFor(bigNumbers[0])).to.equal(bigNumbers[1]);
+    });
+    
+    it('should work with the first greater numbers', () => {
+        expect(maxPairWiseProduct.fastFor(firstGreater[0])).to.equal(firstGreater[1]);
+    });
+    
+    it('should work with huge dataset', () => {
+        expect(maxPairWiseProduct.fastFor(hugeDataset[0])).to.equal(hugeDataset[1]);
+    }).timeout(10000);
 
 });
 
 
+
+describe('maxPairWiseProductFastWhile', () => {
+
+    it('should work on simplest case', () => {
+        expect(maxPairWiseProduct.fastWhile(simplestCase[0])).to.equal(simplestCase[1]);
+    });
+
+    it('should work on duplicate inputs', () => {
+        expect(maxPairWiseProduct.fastWhile(duplicate[0])).to.equal(duplicate[1]);
+    });
+
+    it('should work with big numbers', () => {
+        expect(maxPairWiseProduct.fastWhile(bigNumbers[0])).to.equal(bigNumbers[1]);
+    });
+    
+    it('should work with the first greater numbers', () => {
+        expect(maxPairWiseProduct.fastWhile(firstGreater[0])).to.equal(firstGreater[1]);
+    });
+    
+    it('should work with huge dataset', () => {
+        expect(maxPairWiseProduct.fastWhile(hugeDataset[0])).to.equal(hugeDataset[1]);
+    }).timeout(10000);
+
+});
